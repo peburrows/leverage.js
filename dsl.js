7c3087a737a3e5e0177ed77820459bfeb85a4aa8
Phil Burrows <github@philburrows.com>
2012-10-05 11:43:47 -0600

    Pluralization fixes.

--- dsl.js
+++ dsl.js
@@ -1,4 +1,4 @@
-var UserController = new Leverage.Controller.extend({
+var UsersController = new Leverage.Controller.extend({
   events: { 'a.href click' : 'clickHandler' },
   clickHandler: function(event, el){},
 
@@ -16,19 +16,19 @@ Router
 
   // resources
   .resources('users', {
-    show:   function(request){ UserController.show(request.params.id, request); },
-    index:  function(request){ UserController.index(request); },
-    search: function(request){ UserController.search(request.params.q, request); },
+    show:   function(request){ UsersController.show(request.params.id, request); },
+    index:  function(request){ UsersController.index(request); },
+    search: function(request){ UsersController.search(request.params.q, request); },
   })
 
   // alternatively, we could just pass the UserController to the resource definition
-  .resources('users', UserController)
+  .resources('users', UsersController)
 
   // and you can specify constraints and/or additions
-  .resources('users', UserController, {only: ['index', 'show'], collection: ['search']})
+  .resources('users', UsersController, {only: ['index', 'show'], collection: ['search']})
 
   // or, we could infer what is meant
-  .resources('user') // this would call window['UserController'][action] for each matched route
+  .resource('user') // this would call window['UserController'][action] for each matched route
 
   .catchAll(function(){
     // pass the request to your handler
