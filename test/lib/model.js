d1aad4f54dd73647fb9165447bccbc918d6bfd76
Phil Burrows <peburrows@gmail.com>
2012-11-20 00:56:36 -0700

    add key path getting to Model#get note: not all the tests pass yet

--- test/lib/model.js
+++ test/lib/model.js
@@ -1,21 +1,41 @@
 
 describe('Leverage.Model', function() {
   var User, fullName;
-  fullName = function() {};
+  fullName = function() {
+    return "" + this.firstName + " " + this.lastName;
+  };
   User = Leverage.Model.extend({
-    fullName: fullName.boundTo('firstName', 'lastName')
+    fullName: fullName.boundTo('firstName', 'lastName'),
+    nameObj: function() {
+      return {
+        first: this.firstName,
+        last: this.lastName
+      };
+    }
   });
   beforeEach(function() {
-    this.user = new User;
+    this.user = new User({
+      firstName: 'Jimmy',
+      lastName: 'Allen'
+    });
     return spyOn(this.user, 'trigger').andCallThrough();
   });
   return describe('an instance', function() {
     it("should have an id assigned", function() {
       return expect(this.user._leverageID).toBeDefined();
     });
-    it("should allow the getting of attributes", function() {
-      this.user.set('firstName', 'Phil');
-      return expect(this.user.get('firstName')).toEqual('Phil');
+    describe('#get', function() {
+      it("should allow the getting of attributes", function() {
+        this.user.set('firstName', 'Phil');
+        return expect(this.user.get('firstName')).toEqual('Phil');
+      });
+      it("should allow the getting of functions", function() {
+        return expect(this.user.get('fullName()')).toEqual(this.user.fullName());
+      });
+      return it("should allow the getting of attributes by way of a function", function() {
+        expect(this.user.get('nameObj().first')).toEqual(this.user.firstName);
+        return expect(this.user.get('nameObj().last')).toEqual(this.user.lastName);
+      });
     });
     it("should allow setting of attributes", function() {
       this.user.set('firstName', 'Phil');
--- test/src/model.coffee
+++ test/src/model.coffee
@@ -1,20 +1,29 @@
 describe 'Leverage.Model', ->
-  fullName = ->
+  fullName = -> "#{@firstName} #{@lastName}"
   User = Leverage.Model.extend {
     fullName: fullName.boundTo('firstName', 'lastName')
+    nameObj:  -> {first: @firstName, last: @lastName}
   }
 
   beforeEach ->
-    @user = new User
+    @user = new User(firstName: 'Jimmy', lastName: 'Allen')
     spyOn(@user, 'trigger').andCallThrough()
 
   describe 'an instance', ->
     it "should have an id assigned", ->
       expect(@user._leverageID).toBeDefined()
 
-    it "should allow the getting of attributes", ->
-      @user.set('firstName', 'Phil')
-      expect(@user.get('firstName')).toEqual('Phil')
+    describe '#get', ->
+      it "should allow the getting of attributes", ->
+        @user.set('firstName', 'Phil')
+        expect(@user.get('firstName')).toEqual('Phil')
+
+      it "should allow the getting of functions", ->
+        expect(@user.get('fullName()')).toEqual(@user.fullName())
+
+      it "should allow the getting of attributes by way of a function", ->
+        expect(@user.get('nameObj().first')).toEqual(@user.firstName)
+        expect(@user.get('nameObj().last')).toEqual(@user.lastName)
 
     it "should allow setting of attributes", ->
       @user.set('firstName', 'Phil')
