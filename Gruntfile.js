f04681868f94a58a8441ffd3d9999174f32ea777
Phil Burrows <peburrows@gmail.com>
2013-04-30 16:49:52 -0500

    remove old, commented-out config in the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,62 +106,6 @@ module.exports = function(grunt) {
         }
       }
     }
-
-
-    // jasmine: {
-    //   src: ['underscore.1.3.3.min.js', 'jquery.1.8.3.min.js', 'leverage.js'],
-    //   specs : 'test/lib/*.js',
-    //   template: 'test/template.tmpl',
-    //   helpers: ['test/lib/jasmine/jasmine-jquery.js']
-    // },
-
-    // concat: {
-    //   dist: {
-    //     src: ['<banner:meta.banner>', 'lib/leverage.js', 'lib/utils.js', 'lib/validations.js', 'lib/callbacks.js', 'lib/events.js', 'lib/class.js', 'lib/**/*.js'],
-    //     dest: 'leverage.js'
-    //   }
-    // },
-    // min: {
-    //   dist: {
-    //     src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-    //     dest: 'leverage.min.js'
-    //   }
-    // },
-    // watch: {
-    //   tests: {
-    //     files: 'test/src/**/*.coffee',
-    //     tasks: ['coffee:tests', 'test']
-    //   },
-    //   app: {
-    //     files: '<config:lint.files>',
-    //     tasks: 'concat'
-    //   }
-    // },
-    // jshint: {
-    //   options: {
-    //     curly: true,
-    //     eqeqeq: true,
-    //     immed: true,
-    //     laxcomma: true,
-    //     latedef: true,
-    //     newcap: true,
-    //     noarg: true,
-    //     sub: true,
-    //     undef: true,
-    //     boss: true,
-    //     eqnull: true,
-    //     browser: true,
-    //     jquery: true,
-    //     loopfunc: true,
-    //     evil: true
-    //   },
-    //   globals: {
-    //     '_': false,
-    //     'Leverage': false,
-    //     'console': false
-    //   }
-    // },
-    // uglify: {}
   });
 
 
